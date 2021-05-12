244ee96c0895d1b40cb4b7e143512aec17ef83da
Margulan <turganbekmargulan@mail.ru>
2021-05-12 23:45:00 +0600

    -

--- studybot2.py
+++ studybot2.py
@@ -9,7 +9,7 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
-client = MyClient()
-client.run('ODQyMDQyMDQ1MjI5MDM5NjU5.YJviww.fTPNLBP9vW8U8R1tFaVUZy29k4Q')
-
 token = os.environ.get('BOT_TOKEN')
+
+client = MyClient()
+client.run(str(token))
