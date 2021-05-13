e0a5254a631123d018feb841c9148ad5542ee451
Margulan <turganbekmargulan@mail.ru>
2021-05-13 20:50:56 +0600

    -

--- studybot2.py
+++ studybot2.py
@@ -1,15 +1,35 @@
 import discord
-
+from discord.ext import commands
 import os
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('logged on as {0}'.format(self.user))
+intents = discord.Intents.default()
+intents.members = True
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
 
-token = os.environ.get('BOT_TOKEN')
+bot = commands.Bot(command_prefix='?', intents =intents)
 
-client = MyClient()
-client.run(str(token))
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    
+# class MyClient(discord.Client):
+#     async def on_ready(self):
+#         print('logged on as {0}'.format(self.user))
+
+#     async def on_message(self, message):
+#         print('Message from {0.author}: {0.content}'.format(message))
+
+#token = os.environ.get('BOT_TOKEN')
+
+token = "ODQyMDQyMDQ1MjI5MDM5NjU5.YJviww.fTPNLBP9vW8U8R1tFaVUZy29k4Q"
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+#client = MyClient()
+bot.run(str(token))
