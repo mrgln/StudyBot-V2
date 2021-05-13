28f2bfe7d91b1cdc6105099cf39754d00e471752
Margulan <turganbekmargulan@mail.ru>
2021-05-13 22:29:01 +0600

    -

--- studybot2.py
+++ studybot2.py
@@ -1,95 +1,20 @@
 import discord
-from discord.ext import commands
-
 import os
 
-bot = commands.Bot(command_prefix='$')
+client = discord.Client()
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('logged on as {0}'.format(self.user))
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-@bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
-    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
 token = os.environ.get('BOT_TOKEN')
 
-
-client = MyClient()
 client.run(str(token))
-
-# import discord
-# from discord.ext import commands
-# import random
-
-# import os
-
-# description = '''An example bot to showcase the discord.ext.commands extension
-# module.
-# There are a number of utility commands being showcased here.'''
-
-# intents = discord.Intents.default()
-# intents.members = True
-
-# bot = commands.Bot(command_prefix='?', description=description, intents=intents)
-
-# @bot.event
-# async def on_ready():
-#     print('Logged in as')
-#     print(bot.user.name)
-#     print(bot.user.id)
-#     print('------')
-
-# @bot.command()
-# async def add(ctx, left: int, right: int):
-#     """Adds two numbers together."""
-#     await ctx.send(left + right)
-
-# @bot.command()
-# async def roll(ctx, dice: str):
-#     """Rolls a dice in NdN format."""
-#     try:
-#         rolls, limit = map(int, dice.split('d'))
-#     except Exception:
-#         await ctx.send('Format has to be in NdN!')
-#         return
-
-#     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-#     await ctx.send(result)
-
-# @bot.command(description='For when you wanna settle the score some other way')
-# async def choose(ctx, *choices: str):
-#     """Chooses between multiple choices."""
-#     await ctx.send(random.choice(choices))
-
-# @bot.command()
-# async def repeat(ctx, times: int, content='repeating...'):
-#     """Repeats a message multiple times."""
-#     for i in range(times):
-#         await ctx.send(content)
-
-# @bot.command()
-# async def joined(ctx, member: discord.Member):
-#     """Says when a member joined."""
-#     await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
-
-# @bot.group()
-# async def cool(ctx):
-#     """Says if a user is cool.
-#     In reality this just checks if a subcommand is being invoked.
-#     """
-#     if ctx.invoked_subcommand is None:
-#         await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
-
-# @cool.command(name='bot')
-# async def _bot(ctx):
-#     """Is the bot cool?"""
-#     await ctx.send('Yes, the bot is cool.')
-
-# token = os.environ.get('BOT_TOKEN')
-
-# bot.run(str(token))
