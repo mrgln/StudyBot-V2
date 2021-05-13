dc095f3b53477e65532d7b0df8ace8d297523f91
Margulan <turganbekmargulan@mail.ru>
2021-05-14 00:12:35 +0600

    -

--- studybot2.py
+++ studybot2.py
@@ -1,20 +1,35 @@
-import discord
+# import discord
+
+# import os
+
+# client = discord.Client()
+
+# @client.event
+# async def on_ready():
+#     print('We have logged in as {0.user}'.format(client))
+
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
+
+    
+
+# token = os.environ.get('BOT_TOKEN')
+
+# client.run(str(token))
 import os
+import discord
+from discord.ext import commands
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
+studybot = commands.Bot(command_prefix='=')
 token = os.environ.get('BOT_TOKEN')
 
-client.run(str(token))
+@studybot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+studybot.run(str(token))
