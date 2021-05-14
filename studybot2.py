d1223914f9856a76853ef17063f295f61d83fa57
Margulan <turganbekmargulan@mail.ru>
2021-05-15 01:42:21 +0600

    info and help commands added

--- studybot2.py
+++ studybot2.py
@@ -1,33 +1,20 @@
-# import discord
-
-# import os
-
-# client = discord.Client()
-
-# @client.event
-# async def on_ready():
-#     print('We have logged in as {0.user}'.format(client))
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
-
-    
-
-# token = os.environ.get('BOT_TOKEN')
-
-# client.run(str(token))
 import os
 import random
+import datetime
 import discord
 from discord.ext import commands
 
-studybot = commands.Bot(command_prefix='=')
+description = "can stay alive without mrgln"
+
+studybot = commands.Bot(
+    command_prefix='=',
+    description = description,
+    help_command = None
+    )
+
 token = os.environ.get('BOT_TOKEN')
+
+
 
 @studybot.command()
 async def ping(ctx):
@@ -38,8 +25,51 @@ async def ping(ctx):
     else:
         await ctx.send('pong 🏓')
 
+
+@studybot.command()
+async def rnd(ctx, a: int, b: int):
+    """☄️ Finds random number for you."""
+    number = random.randint(a,b)
+    await ctx.send(number)
+
+
+@studybot.command()
+async def math(ctx, left: int, right: int):
+    """☄️ Adds two numbers together."""
+    await ctx.send(left + right)
+
+
 @studybot.command()
 async def img(ctx):
-    await ctx.send('https://wmpics.pics/di-PPUG.png')
+    await ctx.send('hello https://wmpics.pics/di-PPUG.png')
 
+
+
+@studybot.command()
+async def help(ctx):
+    embed = discord.Embed(title="StudyBot V2", timestamp=datetime.datetime.utcnow(), color=discord.Color.red())
+    embed.add_field(name="🎲 Fun Commands", value="`ping` `rnd`", inline=False)
+    embed.add_field(name="🖩 Math Commands", value="`math`",inline=False)
+    embed.add_field(name="📋 Schedule Commands", value="fds",inline=False)
+    embed.set_image(url='https://avatanplus.ru/files/resources/original/58dd307a43fb515b20055da6.jpg')
+    embed.set_footer(text='made by mrgln with <3')
+    #embed.set_thumbnail(url="https://pluralsight.imgix.net/paths/python-7be70baaac.png")
+    await ctx.send(embed=embed)
+
+
+
+
+@studybot.command()
+async def info(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="=)", timestamp=datetime.datetime.utcnow(), color=discord.Color.red())
+    embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
+    #embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
+    embed.add_field(name="Server Region", value="Kazakhstan")
+    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
+    # embed.set_thumbnail(url=f"{ctx.guild.icon}")
+    #embed.set_thumbnail(url="https://pluralsight.imgix.net/paths/python-7be70baaac.png")
+    await ctx.send(embed=embed)
+
+
+    
 studybot.run(str(token))
